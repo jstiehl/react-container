efa0cdd206de82ac079dbd6cd27ea8ab8d29c263
James Stiehl <jstiehl@gmail.com>
2019-03-21 14:30:35 -0700

    add bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,35 @@
  * Main App
  */
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>React App Container</h1>
-        <p>I guess this is where I can start putting stuff?</p>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="header">React App Container</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="main">
+            <p>I guess this is where I can start putting stuff?</p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="footer">
+              <Button variant="success">Press Me</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
